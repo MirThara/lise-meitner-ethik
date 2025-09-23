93b6f4c88f434c6e35f0dd179aaf9b2eb5962dd6
MirThara <mi.tha.14@icloud.com>
2025-09-23 13:10:13 +0200

    final version

--- assets/pages/statements/data.js
+++ assets/pages/statements/data.js
@@ -1,13 +1,49 @@
 export const text1 = [
     {
-        heading: "Titel",
-        content: `Lorem <b>ipsum</b> dolor sit <sup>amet</sup> consectetur <sub>adipisicing elit</sub>. <i>Hic</i> numquam recusandae voluptas culpa. Atque dolore harum
-numquam incidunt alias aspernatur quo quos sequi fuga, expedita odio libero aut, modi nemo accusantium cumque officia,
-fugit ipsa pariatur id consequuntur. Veritatis hic eius rem, repellat cupiditate magnam neque voluptatum dolore aut quis
-molestias pariatur beatae minima quia ad doloremque perspiciatis placeat laboriosam inventore quasi voluptates ab omnis
-maiores. Id quidem modi consequatur a et perferendis suscipit natus, eaque possimus sequi amet ipsum, quod doloribus
-veritatis deserunt ex facilis harum quos minus. Similique odit molestias debitis expedita incidunt a eligendi maxime,
-sapiente velit.`
+        heading: "Die Verantwortung der Wissenschaft in unserer Gesellschaft",
+        content: `In unserer Gesellschaft spielt Wissenschaft eine sehr wichtige Rolle. Sie hilft uns nicht nur, die Welt
+um uns immer weiter zu verstehen, sondern setzt damit gleichzeitig die Grundlage für die Entwicklung
+neuer Technologien und ermöglicht so den Fortschritt der Menschheit. Dieser wiederum erleichtert
+uns durch die Lösung verschiedenster Probleme das Leben, und bestimmt so, wie unser
+Zusammenleben funktioniert.
+Dadurch übernehmen Wissenschaftlerinnen und Wissenschaftler Verantwortung für die Gesellschaft,
+indem sie alle zu einem Teil darüber entscheiden, welche Gebiete auf welche Weise erforscht werden
+und damit unserer aller Leben formen. Doch sobald ein einzelner oder eine einzelne von ihnen eine
+Entdeckung macht und veröffentlicht, ist das nicht mehr rückgängig zu machen, da man dieses Wissen
+nicht auslöschen kann. Auf den ersten Blick stellt das zunächst kein Problem dar, weil die Entdeckung
+so für verschiedenste Entwicklungen eingesetzt werden kann.
+Diese Entwicklungen müssen aber nicht zwingend positiv sein, wie auch das Beispiel von Lise Meitner
+mit der Entdeckung der Kernspaltung zeigt. Eigentlich arbeitete sie an unserem Verständnis der Welt
+und trug einen wichtigen Teil zur Entwicklung einer neuartigen Methode der Energiegewinnung bei.
+Gleichzeitig wurde die Entdeckung allerdings auch für den Bau der Atombombe genutzt, die das Ziel
+hatte, möglichst große Zerstörung anzurichten. Dabei finde ich das Zitat von Lise Meitner selbst sehr
+spannend: “Sie dürfen uns Wissenschaftlern nicht die Schuld dafür geben, wie Kriegstechniker unsere
+Entdeckungen nutzen.” Es drückt nämlich aus, dass ihrer Meinung nach letztendlich nicht die
+Wissenschaftlerinnen und Wissenschaftler die Verantwortung dafür tragen, was mit ihren
+Erkenntnissen angestellt wird. Dabei stimme ich ihr prinzipiell zu, denn im Grunde genommen ist
+niemand für das Handeln eines anderen verantwortlich und in diesem Fall haben die Entwickler der
+Atombombe selbst in ihrer Verantwortung einen Fehler gemacht. Diese Ansicht wird auch durch den
+Gedanken unterstützt, dass irgendwann sowieso jemand anderes die gleiche oder eine ähnliche
+Entdeckung machen wird und diese womöglich gar nicht für tatsächlichen Fortschritt, sondern nur für
+etwas wie eben den Bau der Atombombe weitergibt. Dennoch könnte man über dieses Thema
+diskutieren, da ohne die Entdeckung der Kernspaltung zumindest zu diesem Zeitpunkt logischerweise
+auch keine Waffe daraus hätte entwickelt werden können. Auf der anderen Seite hatten die
+Wissenschaftlerinnen und Wissenschaftler in der Regel keine zerstörerische Absicht dahinter und das
+zeigt wieder, wie komplex die Frage der Verantwortung in unserer damaligen und auch heutigen
+Gesellschaft ist.
+Was jedoch klar ist, ist, dass komplett ohne Wissenschaft keinerlei menschlicher Fortschritt möglich
+wäre, und das ist schließlich genau der Punkt, der unsere Spezies von anderen unterscheidet. Deshalb
+müssen wir unsere Wissenschaftlerinnen und Wissenschaftler für ihre wichtige Arbeit ehren. Dazu
+gibt es eine Vielzahl von Preisen, die in unterschiedlichen Gebieten verliehen werden. Hier stellt sich
+aber erneut die Frage der Verantwortung, und zwar wer für die zu ehrende Entdeckung verantwortlich
+war; also wer sie gemacht hat. Beim Beispiel Lise Meitners wurde meiner Meinung nach eindeutig ein
+Fehler bei der Beantwortung dieser Frage gemacht, denn nur Otto Hahn allein erhielt den Nobelpreis
+für die Entdeckung der Kernspaltung, obwohl es Lise Meitner war, die die von ihm und Straßmann
+durchgeführten Versuche zusammen mit ihrem Neffen erstmals deutete. Das lässt sich auch auf die
+damaligen gesellschaftlichen Umstände zurückführen, in denen Frauen stark benachteiligt wurden.
+So wird wieder einmal klar, wie eng Wissenschaft und Gesellschaft miteinander verbunden sind und
+sich gegenseitig beeinflussen. In unserer Verantwortung liegt es nun, in der Gegenwart und Zukunft
+allen einen Zugang zu dieser Wissenschaft zu geben und ihre Leistungen fair zu belohnen.`
     }
 ];
 
